208623d03e94f2060db077d90c3eb6ce4bb91a27
Jake Ryan <jake@springnation.co.uk>
2022-03-14 23:02:04 +0000

    Fix

--- pages/organisation/[organisationId]/course/[courseId]/new/[view].tsx
+++ pages/organisation/[organisationId]/course/[courseId]/new/[view].tsx
@@ -5,10 +5,11 @@ import NewMenuRoot from 'components/NewMenu/NewMenuRoot'
 
 const Page: NextPage = () => {
   let router = useRouter();
-  let view = router.query.view.toString();
+  let view = router.query.view?.toString();
 
   if (view === "event") return <NewMenuRoot menu={"EVENT"} />
   if (view === "resource") return <NewMenuRoot menu={"RESOURCE"} />
+  return <></>;
 }
 
 export default Page
